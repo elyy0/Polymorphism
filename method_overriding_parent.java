27c68c6fc72f838cfd9e9baeea6be34b2b6416ec
ILMA MALIK <26ilmamalik@gmail.com>
2023-08-28 11:33:13 +0530

    method_overriding_parent.java

---
+++ method_overriding_parent.java
@@ -0,0 +1,22 @@
+package Method_Overriding;
+public class method_overriding_parent 
+{
+    public void swapWithThirdVariable(int a,int b) 
+    {
+        System.out.println("Using third variable:");
+        System.out.println("Before swapping: a="+a+",b="+b);
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println("After swapping: a="+a+",b="+b);
+    }
+    public void swapWithoutThirdVariable(int a,int b)
+    {
+        System.out.println("Without using third variable:");
+        System.out.println("Before swapping: a="+a+",b="+b);
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("After swapping: a="+a+",b="+b);
+    }
+}
